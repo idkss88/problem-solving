1efbca225e9b73cf2a5ce51d8850f772f112f69f
initial_kss@naver.com <initial_kss@naver.com>
2022-04-26 01:23:51 +0900

    Add PS 15. 3Sum

--- leetcode/15_3Sum/3sum.cpp
+++ leetcode/15_3Sum/3sum.cpp
@@ -1,36 +1,54 @@
 #include<string>
 #include<vector>
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        vector<vector<int>> ans;
-        
-        for (int i = 0 ; i < nums.size()-2; i++) {
-            
-            vector<int> result;
-            
-            for (int j = i+1 ; j < nums.size()-1; j++ ) {
-                    
-                for (int k = j+1; k < nums.size(); k++) {
-                    
-                    if (0 == (nums[i] + nums[j] + nums[k])) {
-                        cout <<nums[i]<<":"<<nums[j]<<":"<< nums[k]<<endl;
-                        result.push_back(nums[i]);
-                        result.push_back(nums[j]);
-                        result.push_back(nums[k]);
-                        
-                        ans.push_back(result);
-                        result.clear();
-                    } 
-                } 
+        vector<vector<int>> res;
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0 ; i < nums.size(); i++) {
+
+            int target  = -nums[i];
+            int front = i + 1;
+            int back = nums.size() - 1;
+
+
+            if(target < 0)
+            {
+                break;
             }
+
+            while ( front < back ) {
+                int sum = nums[front] + nums[back];
+                if ( sum < target ) {
+                    front++;
+                } else if ( sum > target) {
+                    back--;
+                } else {
+                    vector<int> triplet = {nums[i], nums[front], nums[back]};
+                    res.push_back(triplet);
+
+                    while (front < back && nums[front] == triplet[1]) front++;
+
+                    while (front < back && nums[back] == triplet[2]) back--;
+
+                }
+            }
+
+            while ( i+1 < nums.size() && nums[i+1] == nums[i]) i++;
         }
-        
-        return ans;
+        return res;
     }
 };
+
+int main(void) {
+    vector<int> nums = {-1,0,1,2,-1,-4};
+    Solution s;
+    s.threeSum(nums);
+    return 0;
+}
