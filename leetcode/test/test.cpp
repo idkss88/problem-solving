204d993409c58f13b6d6c67bab5ce237ebbff38f
initial_kss@naver.com <initial_kss@naver.com>
2022-05-27 01:20:28 +0900

    Add PS 30. Substring with Concatenation of All Words [ing]

--- leetcode/test/test.cpp
+++ leetcode/test/test.cpp
@@ -7,12 +7,48 @@ using namespace std;
 
 int main(void) {
 
+    string test = "abcdefghdabcdefhgk";
     string str[] = {"abc","def","hgk"};
-     unordered_map<string,int> m;
+    unordered_map<string,int> m;
+   
+
+
+
     for (auto& i : str) {
         cout << i <<endl;
          m.insert(make_pair(i,1));
     }
+
+    unordered_map<string,int> remaining = m;
+
+    cout << "size : " << str->size() << endl;
+
+    int length = str->size() * str[0].size();
+
+    int offset = 0;
+    int count = length;
+    while ( offset < test.size()) {
+        cout << "find : " << test.substr(offset,3) << endl;
+        if (remaining.find(test.substr(offset,3)) != remaining.end()) {
+             cout << "exsits! : " << test.substr(offset,3) << " remained "<< remaining[test.substr(offset,3)] << endl;
+            if (remaining[test.substr(offset,3)] > 0) {
+                remaining[test.substr(offset,3)]--;
+                cout << "remove it, cur cout " <<  remaining[test.substr(offset,length)] << endl;
+                count -= 3;
+            } else {
+                remaining = m;
+                count = length;
+            }
+        } else {
+                remaining = m;
+                count = length;
+        } 
+        if (count == 0) {
+            cout << "hit!!" << endl;
+        }
+        offset+= 3;
+    }
+
 
     for (auto& i : m) {
         cout << i.first << " "<< i.second << endl;
@@ -24,9 +60,11 @@ int main(void) {
         cout << itr->first  << " "<< itr->second << endl;
     }
 
-   if (m.find("abc") != m.end()) {
-       m["abc"]++;
-       cout << m["abc"] <<endl;
-   }
+    if (m.find("abc") != m.end()) {
+        m["abc"]++;
+        cout << m["abc"] << endl;
+    }
+
+
 
 }
