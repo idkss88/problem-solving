1b49b1ba5a0c37227ab7ec99f0a6147b9629c591
kangsungsu <initial_kss@naver.com>
2023-05-01 23:33:54 +0900

    Add 23.trie

---
+++ warmming_up/Algorithmic_problem_solving_strategies/26_trie/26_1_trie.cpp
@@ -0,0 +1,44 @@
+#include <memory>
+#include <iostream>
+
+const int ALPHABETS = 26;
+int toNumber(char ch) { return ch - 'A'; }
+
+struct TrieNode {
+	TrieNode * children[ALPHABETS];
+	bool terminal;
+	TrieNode() : terminal(false) {
+		memset(children,0, sizeof(children));
+	}
+   ~TrieNode() {
+		for(int i = 0 ; i < ALPHABETS; i++) 
+			if(children[i])
+				delete children[i];
+	}
+
+	void insert(const char* key) {
+		if (*key == 0)
+			terminal = true;
+		else {
+			int next = toNumber(*key);
+			if (children[next] == nullptr)
+				children[next] = new TrieNode();
+			children[next]->insert(key+1);
+		}
+	}
+
+	TrieNode* find(const char* key) {
+		if(*key == 0) return this;
+		int next = toNumber(*key);
+		if(children[next] == nullptr) return nullptr;
+		return children[next]->find(key+1);
+	}	
+};
+
+int main() {
+	TrieNode t;
+	t.insert("BET");
+	TrieNode* tmp = t.find("BET");
+	
+	return 0;
+}
